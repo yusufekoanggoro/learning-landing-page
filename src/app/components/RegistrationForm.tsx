51606e7c78f73be7798964a80358f9aa8f819c37
Yusuf Eko Anggoro <anggoroekoyusuf@gmail.com>
2025-04-26 20:49:32 +0700

    updates

--- src/app/components/RegistrationForm.tsx
+++ src/app/components/RegistrationForm.tsx
@@ -70,7 +70,7 @@ export default function RegistrationForm() {
               onChange={(e) => setName(e.target.value)}
               required
               placeholder="Contoh: Yusuf"
-              className="w-full p-3 border border-[#004D40] rounded-lg focus:outline-none focus:ring-2 focus:ring-[#004D40]"
+              className="w-full p-3 border border-[#004D40] rounded-lg focus:outline-none focus:ring-2 focus:ring-[#004D40] placeholder-gray-700"
             />
           </div>
 
@@ -85,7 +85,7 @@ export default function RegistrationForm() {
               onChange={(e) => setWhatsapp(e.target.value)}
               required
               placeholder="Contoh: 081234567890"
-              className="w-full p-3 border border-[#004D40] rounded-lg focus:outline-none focus:ring-2 focus:ring-[#004D40]"
+              className="w-full p-3 border border-[#004D40] rounded-lg focus:outline-none focus:ring-2 focus:ring-[#004D40] placeholder-gray-700"
             />
           </div>
 
